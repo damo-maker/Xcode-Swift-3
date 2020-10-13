8f9771e6e1d6fc473af362a320e2e257073cce5d
damo-maker <damianjohns89@gmail.com>
2020-10-14 10:39:54 +1100

    rename IBOutletProperties project

---
+++ XcodeAndSwift3/WarCardGame/ex_05_IBOutlet_properties/IBOutletProperties/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Damian Johns on 23/05/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
