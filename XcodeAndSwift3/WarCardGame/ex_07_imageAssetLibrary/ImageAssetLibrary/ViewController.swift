20f6e3c14bc54774757433603aa2e6fe8da8e8b7
damo-maker <damianjohns89@gmail.com>
2020-10-14 20:44:38 +1100

    add image assets and button method

--- XcodeAndSwift3/WarCardGame/ex_07_imageAssetLibrary/ImageAssetLibrary/ViewController.swift
+++ XcodeAndSwift3/WarCardGame/ex_07_imageAssetLibrary/ImageAssetLibrary/ViewController.swift
@@ -29,7 +29,8 @@ class ViewController: UIViewController {
     
     @IBAction func playRoundTapped(_sender: UIButton) {
         
-        self.playRoundButton.setTitle("Play Round", for: UIControlState.normal)
+        self.firstCardImageView.image = UIImage(named: "card1")
+        self.secondCardImageView.image = UIImage(named: "card11")
     }
 }
 
