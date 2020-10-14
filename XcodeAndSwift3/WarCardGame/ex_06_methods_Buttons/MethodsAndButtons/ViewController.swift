1e98749d4ea5b4a77e8ba1ee61c7414f482d28aa
damo-maker <damianjohns89@gmail.com>
2020-10-14 16:16:20 +1100

    add MethodsAndButtons project

---
+++ XcodeAndSwift3/WarCardGame/ex_06_methods_Buttons/MethodsAndButtons/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Damian Johns on 23/05/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.playRoundButton.setTitle("Play", for: UIControlState.normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
