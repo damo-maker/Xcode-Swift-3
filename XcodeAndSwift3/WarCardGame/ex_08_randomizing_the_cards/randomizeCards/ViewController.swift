7265a62c9149084fd6528de77ccc14b133c50a6d
damo-maker <damianjohns89@gmail.com>
2020-10-15 13:27:03 +1100

    add randomizeCards project

---
+++ XcodeAndSwift3/WarCardGame/ex_08_randomizing_the_cards/randomizeCards/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Damian Johns on 23/05/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.playRoundButton.setTitle("Play", for: UIControlState.normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playRoundTapped(_sender: UIButton) {
+        
+        self.firstCardImageView.image = UIImage(named: "card1")
+        self.secondCardImageView.image = UIImage(named: "card11")
+    }
+}
+
