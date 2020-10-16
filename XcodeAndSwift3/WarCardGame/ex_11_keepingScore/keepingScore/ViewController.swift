33f0ece9bb0fd57575dde1e123071880473c4d19
damo-maker <damianjohns89@gmail.com>
2020-10-16 13:58:03 +1100

    add keepingScore project

---
+++ XcodeAndSwift3/WarCardGame/ex_11_keepingScore/keepingScore/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Damian Johns on 23/05/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstCardImageView: UIImageView!
+    @IBOutlet weak var secondCardImageView: UIImageView!
+    @IBOutlet weak var playRoundButton: UIButton!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    let cardNames = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.playRoundButton.setTitle("Play", for: UIControlState.normal)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playRoundTapped(_sender: UIButton) {
+        
+        // Randomize left number from 0 to 12
+        let leftNumber = Int(arc4random_uniform(13))
+        
+        // Set the left image
+        self.firstCardImageView.image = UIImage(named: cardNames[leftNumber])
+        
+        // Randomize rightNumber from 0 to 12
+        let rightNumber = Int(arc4random_uniform(13))
+        
+        // Set the right image
+        self.secondCardImageView.image = UIImage(named: cardNames[rightNumber])
+        
+        // Compare the card numbers
+        if leftNumber > rightNumber {
+            // Left card wins
+        } else if leftNumber == rightNumber {
+            // It's a tie
+        } else {
+            // Right card wins
+        }
+    }
+}
+
