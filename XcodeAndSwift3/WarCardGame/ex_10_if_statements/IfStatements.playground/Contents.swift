1a89dd122e072238430f331213d08e6845e682a8
damo-maker <damianjohns89@gmail.com>
2020-10-16 11:03:01 +1100

    add ifStatements project

---
+++ XcodeAndSwift3/WarCardGame/ex_10_if_statements/IfStatements.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a = 13
+
+if a < 10 {
+    print("hello")
+} else if a > 10 && a < 15 {
+    print("are you well?")
+} else {
+    print("goodbye")
+}
