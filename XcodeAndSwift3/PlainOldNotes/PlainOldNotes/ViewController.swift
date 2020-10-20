52535850cc40b9c45bcc2eca5dbbd482de0044ce
damo-maker <damianjohns89@gmail.com>
2020-10-20 21:36:31 +1100

    add PlainOldNotes app

---
+++ XcodeAndSwift3/PlainOldNotes/PlainOldNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlainOldNotes
+//
+//  Created by Damian Johns on 27/04/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
