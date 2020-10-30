41cd4abbf5df915816b49a438c089fb91a8dda52
damo-maker <damianjohns89@gmail.com>
2020-10-30 21:07:44 +1100

    modify layout, add becomeFirstResponder()

--- XcodeAndSwift3/AddNumbers/AddNumbers/ViewController.swift
+++ XcodeAndSwift3/AddNumbers/AddNumbers/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var myTextField1: UITextField!
     @IBOutlet weak var myTextField2: UITextField!
     @IBOutlet weak var myLabel1: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        myTextField1.becomeFirstResponder()
+        myTextField1.delegate = self
         myLabel1.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -26,15 +28,29 @@ class ViewController: UIViewController {
     
     @IBAction func myButton1(_ sender: AnyObject) {
         myLabel1.isHidden = false
+        
+        
         let firstValue = Double(myTextField1.text!)
         let secondValue = Double(myTextField2.text!)
         
         if firstValue != nil && secondValue != nil {
             let outputValue = Double(firstValue! + secondValue!)
-            myLabel1.text = "The answer is \(outputValue)"
+            myLabel1.text = "\(outputValue)"
         } else {
             myLabel1.text = "Hey enter a number buddy!"
         }
+        myTextField1.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        myTextField1.resignFirstResponder()
+     
+    }
+    
+    func textFieldShouldReturn(_ myTextField1: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
     }
     
 }
