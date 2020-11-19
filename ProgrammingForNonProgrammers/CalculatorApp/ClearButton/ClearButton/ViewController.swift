63ba39e05573af3b0b7797fa2e251b9b1f3cd14e
damo-maker <damianjohns89@gmail.com>
2020-11-19 22:22:02 +1100

    add clear button to reset calculator to default setting

---
+++ ProgrammingForNonProgrammers/CalculatorApp/ClearButton/ClearButton/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Damian Johns on 22/04/2020.
+//  Copyright © 2020 Damian Johns. All rights reserved.
+//
+
+import UIKit
+
+enum modes {
+    case notSet
+    case addition
+    case subtraction
+    case multiplication
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString: String = "0"
+    var currentMode: modes = .notSet
+    var savedNum: Int = 0
+    var lastButtonWasMode: Bool = false
+
+    func updateText() {
+        guard let labelInt: Int = Int(labelString) else {
+            label.text = "Error"
+            return
+        }
+        
+        label.text = "\(labelInt)"
+    }
+
+    func changeModes(newMode: modes) {
+        
+    }
+    
+    @IBAction func didPressPlus(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func didPressMinus(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func didPressEquals(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func didPressClear(_ sender: AnyObject) {
+        labelString = "0"
+        currentMode = .notSet
+        savedNum = 0
+        lastButtonWasMode = false
+        label.text = "0"
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        guard let stringValue: String = sender.titleLabel?.text else {
+            label.text = "Error"
+            return
+        }
+        
+        labelString = labelString.appending(stringValue)
+        updateText()
+        
+    }
+    
+}
+
